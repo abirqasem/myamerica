58a470b7d7683964d14fdbc1d62464207f059976
tanyagupta <gupta.tanya@gmail.com>
2024-05-18 12:39:19 -0400

    feat: updated to correct errors

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # myamerica.life
 
-### citizenship quiz flashcard and other cool projects 
+### citizenship quiz flashcard and other cool projects
 
 
-This is a google Appengine project. The app is running in gcloud. 
+This is a google Appengine project. The app is running in gcloud.
 
 To locally run the app clone the repository, `cd` into the root directory of the project (where the `app.yaml`) file is located, then type
 
@@ -11,4 +11,21 @@ To locally run the app clone the repository, `cd` into the root directory of the
 
 This is should work in any cloud 9 workspace or in a local machine.
 
+May 17 2024 - followed the following instructions:
 
+https://cloud.google.com/appengine/docs/standard/python3/services/access
+
+just up to migration considerations section
+
+May 18 2024
+
+Security errors when using legacy bundled services for Python
+If you use a legacy bundled services API when a Python 3 app is starting up, you might see the following error message:
+
+
+Attempted RPC call without active security ticket
+This error can occur in scenarios such as reading certain values from Memcache when your app is starting to configure a database connection or set a global variable.
+
+To resolve this issue, you could try moving such logic into a warmup request.
+
+https://cloud.google.com/appengine/docs/standard/troubleshooting
--- app.py
+++ app.py
@@ -1,8 +1,29 @@
 import logging
 
-from google.appengine.api import urlfetch
+import google
+import sys
+import logging
+import requests
+from requests import get
+import urllib.request
+
 
 from flask import Flask, render_template, request
+
+from google.appengine.api import wrap_wsgi_app
+from google.appengine.api import urlfetch as urlfetch
+
+
+app = Flask(__name__)
+app.wsgi_app = wrap_wsgi_app(app.wsgi_app,use_legacy_context_mode=True, use_deferred=True and False)
+
+#os.environ["PATH"] += os.pathsep + app_path
+# gae_dir = google.__path__.append('/Users/tanyagupta/.local/share/virtualenvs/myamerica-_5IkJJdw/lib/python3.11/site-packages/google')
+# sys.path.insert(0, gae_dir) # might not be necessary
+# import google.appengine # now it's on your import path`
+#from google.appengine.api import urlfetch
+
+#from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -30,7 +51,8 @@ def get_pols():
     state = request.args.get ('state')
 
     try:
-        result = urlfetch.fetch("https://script.google.com/macros/s/AKfycbypEMJ3kW4juwLOp3iPod4fWjinezYGRFnJQYnw-WOiBHHkExw/exec?who="+str(who)+"&state="+str(state))
+        #result = google.appengine.api.urlfetch.create_rpc(deadline=None, callback=None)
+        result = requests.get("https://script.google.com/macros/s/AKfycbypEMJ3kW4juwLOp3iPod4fWjinezYGRFnJQYnw-WOiBHHkExw/exec?who="+str(who)+"&state="+str(state))
         if result.status_code == 200:
            pols = result.content
         else:
@@ -45,14 +67,26 @@ def get_services():
     services = {}
     what = request.args.get ('what')
 
+
+
     try:
-        result = urlfetch.fetch("https://script.google.com/macros/s/AKfycbypEMJ3kW4juwLOp3iPod4fWjinezYGRFnJQYnw-WOiBHHkExw/exec?resources="+str(what))
+        #result = urlfetch.fetch("https://script.google.com/macros/s/AKfycbypEMJ3kW4juwLOp3iPod4fWjinezYGRFnJQYnw-WOiBHHkExw/exec?resources="+str(what))
+        #rpc = urlfetch.create_rpc()
+        #urlfetch.make_fetch_call(rpc,"https://script.google.com/macros/s/AKfycbypEMJ3kW4juwLOp3iPod4fWjinezYGRFnJQYnw-WOiBHHkExw/exec?resources="+str(what))
+        result = requests.get("https://script.google.com/macros/s/AKfycbypEMJ3kW4juwLOp3iPod4fWjinezYGRFnJQYnw-WOiBHHkExw/exec?resources="+str(what))
+        #result = rpc.get_resut()
+    #     self.response.write(result.read())
+    # except urllib.error.URLError:
+    #         logging.exception('Caught exception fetching url')
+    #     return services
         if result.status_code == 200:
            services = result.content
         else:
             services=result.status_code
     except urlfetch.Error:
         logging.exception('Caught exception fetching url')
+        # NameError: name 'GET' is not defined
+         #Traceback (most recent call last):    File "/layers/google.python.pip/pip/lib/python3.9/site-packages/flask/app.py", line 1473, in wsgi_app      response = self.full_dispatch_request()    File "/layers/google.python.pip/pip/lib/python3.9/site-packages/flask/app.py", line 882, in full_dispatch_request      rv = self.handle_user_exception(e)    File "/layers/google.python.pip/pip/lib/python3.9/site-packages/flask/app.py", line 880, in full_dispatch_request      rv = #self.dispatch_request()    File "/layers/google.python.pip/pip/lib/python3.9/site-packages/flask/app.py", line 865, in dispatch_request      return self.ensure_sync(self.view_functions[rule.endpoint])(**view_args)  # type: ignore[no-any-return]    File "/workspace/app.py", line 72, in get_services      result = urlfetch.fetch("https://script.google.com/macros/s/AKfycbypEMJ3kW4juwLOp3iPod4fWjinezYGRFnJQYnw-WOiBHHkExw/exec?resources="+str(what),payload=None,method=GET,headers={},allow_truncated=False,follow_redirects=True,deadline=None,validate_certificate=None)  NameError: name 'GET' is not defined
 
     return services
 
